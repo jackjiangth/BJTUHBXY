029ceefd4619fec246c60b8ce9f63ca2bbb750ff
lanou3g <378291314@qq.com>
2016-04-23 10:45:28 +0800

    四个button,

--- BJTUHBXY/BJTUHBXY/HomePage/ViewControllers/CollectionDetailViewController.m
+++ BJTUHBXY/BJTUHBXY/HomePage/ViewControllers/CollectionDetailViewController.m
@@ -16,9 +16,37 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    self.view.backgroundColor = [UIColor colorWithRed:0.5 green:0.7 blue:0.6 alpha:0.4];
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithTitle:@"返回" style:UIBarButtonItemStylePlain target:self action:@selector(back:)];
+    switch (self.follow) {
+        case 0:
+            self.view.backgroundColor = [UIColor redColor];
+            break;
+            
+        case 1:
+            self.view.backgroundColor = [UIColor orangeColor];
+            break;
+        case 2:
+            self.view.backgroundColor = [UIColor greenColor];
+            break;
+        case 3:
+            self.view.backgroundColor = [UIColor cyanColor];
+            break;
+            
+        default:
+            break;
+    }
+
+
 }
 
+
+-(void)back:(id)sender{
+    [self dismissViewControllerAnimated:YES completion:^{
+        NSLog(@"成功返回上一级视图.哈哈哈");
+    }];
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
--- BJTUHBXY/BJTUHBXY/HomePage/ViewControllers/HomePageViewController.m
+++ BJTUHBXY/BJTUHBXY/HomePage/ViewControllers/HomePageViewController.m
@@ -10,7 +10,16 @@
 #import "SDCycleScrollView.h"
 #import "QRViewController.h"
 #import "DetailTableViewController.h"
+#import "CollectionDetailViewController.h"
+
+
+#define k self.view.frame.size.width;
+#define h self.view.frame.size.height;
+
 @interface HomePageViewController ()<SDCycleScrollViewDelegate>
+
+
+
 
 @end
 
@@ -20,7 +29,7 @@
     [super viewDidLoad];
     self.navigationController.navigationBar.backgroundColor = [UIColor orangeColor];
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithImage:[UIImage imageNamed:@"nav_menu_icon.png"] style:UIBarButtonItemStylePlain target:self action:@selector(US:)];
-
+  
     
     self.view.backgroundColor = [UIColor clearColor];
 //    UIImageView *backgroundView=[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"1.jpg"]];
@@ -44,10 +53,25 @@
     cycleScrollView.bannerImageViewContentMode = UIViewContentModeScaleToFill;
     cycleScrollView.currentPageDotColor=[UIColor whiteColor];
     [demoContentView addSubview:cycleScrollView];
-   //模拟延迟加载
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.0001 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        cycleScrollView.imageURLStringsGroup = imageURLStrings;
-    });
+   cycleScrollView.imageURLStringsGroup = imageURLStrings;
+//    //模拟延迟加载
+//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.0001 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+//        
+//    });
+    
+    
+    
+    NSArray *arr = [NSArray arrayWithObjects:[UIColor colorWithRed:arc4random()%256/255.0 green:arc4random()%256/255.0 blue:arc4random()%256/255.0 alpha:0.5],[UIColor colorWithRed:arc4random()%256/255.0 green:arc4random()%256/255.0 blue:arc4random()%256/255.0 alpha:0.5],[UIColor colorWithRed:arc4random()%256/255.0 green:arc4random()%256/255.0 blue:arc4random()%256/255.0 alpha:0.5],[UIColor colorWithRed:arc4random()%256/255.0 green:arc4random()%256/255.0 blue:arc4random()%256/255.0 alpha:0.5],nil];
+    
+    self.button1=[[UIButton alloc]initWithFrame:CGRectMake(50, 400, 50, 50)];
+    for (int i = 0; i < 4; i++) {
+        UIButton *button = [[UIButton alloc]initWithFrame:CGRectMake(50+100*i, 400, 50, 50)];
+        [button setTag:i+1];
+        button.backgroundColor = arr[i];
+        [button addTarget:self action:@selector(show:) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:button];
+        
+    }
     
 }
 
@@ -56,8 +80,22 @@
     
     QRViewController *qr = [[QRViewController alloc]init];
     [self.navigationController pushViewController:qr animated:YES];
-   
+    
 }
+
+
+-(void)show:(UIButton*)sender{
+    CollectionDetailViewController *collect=[[CollectionDetailViewController alloc]init];
+    collect.follow = sender.tag;
+    UINavigationController *na=[[UINavigationController alloc]initWithRootViewController:collect];
+    NSLog(@"%ld",sender.tag);
+    [self.view.window.rootViewController presentViewController:na animated:YES completion:nil];
+   
+
+}
+
+
+
 #pragma mark - SDCycleScrollViewDelegate
 
 - (void)cycleScrollView:(SDCycleScrollView *)cycleScrollView didSelectItemAtIndex:(NSInteger)index
@@ -74,6 +112,27 @@
      
      
      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
